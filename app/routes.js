80747d439a1c19699006afa106c017c36d004065
DANS-LAPTOP\daniel <danielbroadbent87@gmail.com>
2016-12-09 22:37:09 +0000

    all files now staged

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,9 @@
 ////routes///
 
+/// gravtar ///
+var gravatar = require('nodejs-gravatar');
+///var gravatar = require('nodejs-gravatar');
+
 // require express
 var express = require('express');
 var path = require('path');
@@ -16,8 +20,17 @@ router.get('/', function(req, res) {
 
 //route our about
 router.get('/about', function(req, res) {
-	res.render('pages/about');
+	var users = [
+		{name: 'dan', email: 'danielbroadbent87@gmail.com', avatar: ""},
+		{name: 'ant', email:'anthonybroadbent1@gmail.com', avatar: ""},
+	];
+	for (user of users){
+		user.avatar = gravatar.imageUrl(user.email);
+	};
+	res.render('pages/about', {users: users });
 });
+
+	
 
 //route our contact
 router.get('/contact', function(req, res) {
@@ -25,5 +38,7 @@ router.get('/contact', function(req, res) {
 });
 
 router.post('/contact', function(req, res){
-
+	res.send('Thanks for contacting us, ' + req.body.name +  ' we will reply very soon....:)')
+console.log(req.body.message);
 });
+
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 /////express/////
 var express = require('express');
+var nodemailer = require("nodemailer");
 var expressLayouts = require('express-ejs-layouts');
+var bodyParser =require('body-parser');
 var app = express();
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "danielbroadbent87@gmail.com",
+        pass: "Kaleb2010"
+    }
+});
 var port = 8080;
+var http = require("http");
 
-//route our app
-var router = require('./app/routes')
-app.use('/', router);
 
 //use ejs and express layouts
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+//use body parser
+app.use(bodyParser.urlencoded());
+
+//route our app
+var router = require('./app/routes')
+app.use('/', router);
 
 //set static files (css and iamges etc) location
 app.use(express.static(__dirname + '/public'));
@@ -18,5 +32,40 @@ app.use(express.static(__dirname + '/public'));
 //start server
 app.listen(port, function(){
 	console.log('app started')
+});
 
+
+
+
+
+
+////
+
+/// message received
+
+app.get('/',function(req,res){
+    res.sendfile('index.ejs');
+});
+app.get('/send',function(req,res){
+    var mailOptions={
+        to : req.query.to,
+        subject : req.query.subject,
+        text : req.query.text
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});
+
+/*--------------------Routing Over----------------------------*/
+
+app.listen(3000,function(){
+    console.log("Express Started on Port 3000");
 });
